3ca020fdb5f1fbafdfe7802085406f5c831206b6
tuyoleni <simeondevs@gmail.com>
2025-02-15 22:39:23 +0200

    bug fixes: Deployment issues projects page dynaamic import..

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,17 +1,29 @@
 'use client'
 
-import WorldSolarPlantSection from "@/components/sections/world-solar-plant.section";
-import GolmudChinaSection from "@/components/sections/golmud-china.section";
-import BusinessCaseDevelopmentSection from "@/components/sections/business-case-development.section";
-import TrekkopjeMineralRightsSection from "@/components/sections/trekkopje-mineral-rights.section";
+import dynamic from 'next/dynamic';
 
-export default function Projects() {
+// Dynamically import components with no SSR
+const ReferencesSection = dynamic(() => import("@/components/sections/references.section"), { ssr: false });
+const BreakthroughSection = dynamic(() => import("@/components/sections/breakthrough.section"), { ssr: false });
+const PowerhouseSection = dynamic(() => import("@/components/sections/powerhouse.section"), { ssr: false });
+const WorldSolarPlantSection = dynamic(() => import("@/components/sections/world-solar-plant.section"), { ssr: false });
+const GolmudChinaSection = dynamic(() => import("@/components/sections/golmud-china.section"), { ssr: false });
+const BusinessCaseDevelopmentSection = dynamic(() => import("@/components/sections/business-case-development.section"), { ssr: false });
+const TrekkopjeMineralRightsSection = dynamic(() => import("@/components/sections/trekkopje-mineral-rights.section"), { ssr: false });
+
+export default function ProjectsPage() {
     return (
         <main>
+            <ReferencesSection />
             <WorldSolarPlantSection />
             <GolmudChinaSection />
-            <BusinessCaseDevelopmentSection />
+            <BreakthroughSection />
+            <PowerhouseSection />
             <TrekkopjeMineralRightsSection />
+            {/* <ProductionCostSection /> */}
+            {/* <RenewableFutureSection /> */}
+            <BusinessCaseDevelopmentSection />
+            {/* <DesalinationPlantSection /> */}
         </main>
     );
 }
