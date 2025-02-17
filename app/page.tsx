281c28dd4d23aeb21328535580b7396bc5518674
tuyoleni <simeondevs@gmail.com>
2025-02-17 16:08:46 +0200

    feature

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import HeroSection from "@/components/sections/hero.section";
 import EnergySection from "@/components/sections/energy.section";
 import BusinessCaseSection from "@/components/sections/business-case.section";
 import QualitySection from "@/components/sections/quality.section";
-import Power2XBlocksSection from "@/components/sections/power2x-blocks.section";
+// import Power2XBlocksSection from "@/components/sections/power2x-blocks.section";
 import ProductionCostSection from "@/components/sections/production-cost.section";
 // import RenewableFutureSection from "@/components/sections/renewable-future.section";
 // import DesalinationPlantSection from "@/components/sections/desalination-plant.section";
